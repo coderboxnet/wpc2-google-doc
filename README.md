868a16e07e451ec362076e6979648d925e78c5e2
Javier Moran <javiery2k@gmail.com>
2024-06-24 18:58:03 -0500

    First Commit. Using composer to create a plugin in Wordpress

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # wpc2-google-doc
 A WordPress plugin to backup posts as Google Docs
+
+STEP 1:
+composer install
---
+++ src/Plugin.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Plugin Class.
+ *
+ * @package wpc2-google-doc
+ */
+
+namespace Wpc2GoogleDoc;
+
+/**
+ * Class Plugin.
+ */
+class Plugin {
+
+	/**
+	 * Constructor.
+	 */
+	public function __construct() {
+		$this->init();
+	}
+
+	/**
+	 * Activate plugin
+	 */
+	public function activate() {}
+	
+	/**
+	 * Deactivate plugin
+	 */
+	public function deactivate() {}
+
+	/**
+	 * Initialize plugin
+	 */
+	private function init() {}
+}
